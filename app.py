cc88dcca4000e2c21b3eed7da52ad1fa8d97beb7
karthiksivala3223 <karthiksivala2000@gmail.com>
2026-03-31 15:11:28 +0530

    added some code app.py

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ print(f"Hello, {name}!")
 numbers = [1, 2, 3, 4]
 squares = [n**2 for n in numbers]
 print(squares)  # Output: [1, 4, 9, 16]
+
+def factorial(n):
+if n == 0:
+return 1
+else:
+return n * factorial(n-1)
+
+print(factorial(5)) # Output: 120
